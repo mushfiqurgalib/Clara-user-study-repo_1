8592b8e2a5ce6cd8a0ac79ec8c0103af792efed6
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2025-07-13 01:27:56 +0600

    Update HistogramStatistics.java

--- src/main/java/com/harium/keel/catalano/statistics/HistogramStatistics.java
+++ src/main/java/com/harium/keel/catalano/statistics/HistogramStatistics.java
@@ -1,48 +1,16 @@
-// Catalano Imaging Library
-// The Catalano Framework
-//
-// Copyright © Diego Catalano, 2012-2016
-// diego.catalano at live.com
-//
-// Copyright © Andrew Kirillov, 2007-2008
-// andrew.kirillov at gmail.com
-//
-//    This library is free software; you can redistribute it and/or
-//    apply it under the terms of the GNU Lesser General Public
-//    License as published by the Free Software Foundation; either
-//    version 2.1 of the License, or (at your option) any later version.
-//
-//    This library is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//    Lesser General Public License for more details.
-//
-//    You should have received a copy of the GNU Lesser General Public
-//    License along with this library; if not, write to the Free Software
-//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-//
+
 
 package com.harium.keel.catalano.statistics;
 
 
 import com.harium.keel.catalano.core.IntRange;
 
-/**
- * Set of histogram statistics functions. 
- * @author Diego Catalano
- */
 public final class HistogramStatistics {
 
-    /**
-     * Don't let anyone instantiate this class.
-     */
+   
     private HistogramStatistics() {}
     
-    /**
-     * Calculate entropy value.
-     * @param values Values.
-     * @return Returns entropy value of the specified histogram array.
-     */
+  
     public static double Entropy( int[] values ){
         int     n = values.length;
         int     total = 0;
@@ -70,12 +38,7 @@ public final class HistogramStatistics {
         return entropy;
     }
     
-    /**
-     * Get range around median containing specified percentage of values.
-     * @param values Values.
-     * @param percent Values percentage around median.
-     * @return Returns the range which containes specifies percentage of values.
-     */
+
     public static IntRange GetRange( int[] values, double percent ){
         int total = 0, n = values.length;
 
@@ -106,24 +69,14 @@ public final class HistogramStatistics {
         return new IntRange( min, max );
     }
     
-    /**
-     * Calculate Kurtosis value.
-     * @param values Values.
-     * @return Returns kurtosis value of the specified histogram array.
-     */
+ 
     public static double Kurtosis(int[] values){
         double mean = Mean(values);
         double std = StdDev(values, mean);
         return Kurtosis(values, mean, std);
     }
     
-    /**
-     * Calculate Kurtosis value.
-     * @param values Values.
-     * @param mean Specified mean.
-     * @param stdDeviation Specified standard deviation.
-     * @return Returns kurtosis value of the specified histogram array.
-     */
+
     public static double Kurtosis(int[] values, double mean, double stdDeviation){
         double n = 0;
         for (int i = 0; i < values.length; i++)
@@ -143,11 +96,7 @@ public final class HistogramStatistics {
         return part1 * part2 - part3;
     }
     
-    /**
-     * Calculate Mean value.
-     * @param values Values.
-     * @return Mean.
-     */
+
     public static double Mean( int[] values ) {
         int     hits;
         long    total = 0;
@@ -164,11 +113,7 @@ public final class HistogramStatistics {
         return ( total == 0 ) ? 0 : mean / total;
     }
     
-   /**
-     * Calculate Median value.
-     * @param values Values.
-     * @return Median.
-     */
+
     public static int Median( int[] values ){
         int total = 0, n = values.length;
 
@@ -193,11 +138,7 @@ public final class HistogramStatistics {
         return median;
     }
     
-    /**
-     * Calculate Mode value.
-     * @param values Values.
-     * @return Returns mode value of the histogram array.
-     */
+ 
     public static int Mode( int[] values ){
         int mode = 0, curMax = 0;
 
@@ -212,24 +153,14 @@ public final class HistogramStatistics {
         return mode;
     }
     
-    /**
-     * Calculate Skewness value.
-     * @param values Values.
-     * @return Returns skewness value of the specified histogram array.
-     */
+ 
     public static double Skewness(int[] values){
         double mean = Mean(values);
         double std = StdDev(values, mean);
         return Skewness(values, mean, std);
     }
     
-    /**
-     * Calculate Skewness value.
-     * @param values Values.
-     * @param mean Specified mean.
-     * @param stdDeviation Specified standard deviation.
-     * @return Returns skewness value of the specified histogram array.
-     */
+
     public static double Skewness(int[] values, double mean, double stdDeviation){
         double n = 0;
         for (int i = 0; i < values.length; i++)
@@ -245,21 +176,12 @@ public final class HistogramStatistics {
         return part1 * part2;
     }
     
-    /**
-     * Calculate standart deviation.
-     * @param values Values.
-     * @return Standart deviation.
-     */
+   
     public static double StdDev( int[] values ){
         return StdDev( values, Mean( values ) );
     }
     
-    /**
-     * Calculate standart deviation.
-     * @param values Values.
-     * @param mean Mean.
-     * @return Standart deviation.
-     */
+ 
     public static double StdDev( int[] values, double mean ){
         double  stddev = 0;
         double  diff;
